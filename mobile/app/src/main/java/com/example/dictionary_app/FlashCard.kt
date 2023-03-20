718788f5f6b9f277477ec3b6fafa1a4fd085913a
MiniWalls <69449726+MiniWalls@users.noreply.github.com>
2023-03-20 21:59:07 +0200

    Initialized flashcard activity

---
+++ mobile/app/src/main/java/com/example/dictionary_app/FlashCard.kt
@@ -0,0 +1,11 @@
+package com.example.dictionary_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FlashCard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_flash_card)
+    }
+}
--- mobile/app/src/main/java/com/example/dictionary_app/MainActivity.kt
+++ mobile/app/src/main/java/com/example/dictionary_app/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.dictionary_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var buFlashCardActivity = findViewById<Button>(R.id.buFlashCardActivity)
+        buFlashCardActivity.setOnClickListener {
+            val intent = Intent(this, FlashCard::class.java)
+            startActivity(intent)
+        }
     }
 }
