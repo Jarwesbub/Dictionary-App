036560a3d4a13e68b953c30256c8d07cc552416d
Ilari Tuovinen <ilari.tuovinen@hotmail.com>
2023-03-22 20:20:15 +0200

    added payload parsers

--- server/index.js
+++ server/index.js
@@ -5,6 +5,9 @@ const port = 3000;
 
 const app = express();
 
+app.use(express.json()); // function to parse incoming JSON payloads
+app.use(express.urlencoded()); // function to parse incoming URL-encoded payloads
+
 app.get("/", (req, res) => {
   res.status(200).send({ message: "Kukkuluuruu" });
   res.end;
