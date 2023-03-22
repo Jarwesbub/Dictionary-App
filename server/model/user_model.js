1a10c410efe472e1bed9b405b32ecad319565c24
Ilari Tuovinen <ilari.tuovinen@hotmail.com>
2023-03-22 16:20:57 +0200

    crud for user & export

--- server/model/user_model.js
+++ server/model/user_model.js
@@ -9,3 +9,51 @@ getUsers = () => {
     });
   });
 };
+
+getUserByName = (username) => {
+  const query = "SELECT FROM users WHERE user_name=?";
+  return new Promise((resolve, reject) => {
+    database.query(query, [username], (error, result) => {
+      if (error) reject(error);
+      resolve(result);
+    });
+  });
+};
+
+createUser = (username, password) => {
+  const query = "INSERT INTO users (user_name, user_password) VALUES (?,?)";
+  return new Promise((resolve, reject) => {
+    database.query(query, [username, password], (error, result) => {
+      if (error) reject(error);
+      resolve(result);
+    });
+  });
+};
+
+deleteUser = (userId) => {
+  const query = "DELETE FROM users WHERE user_id=?";
+  return new Promise((resolve, reject) => {
+    database.query(query, [userId], (error, result) => {
+      if (error) reject(error);
+      resolve(result);
+    });
+  });
+};
+
+UpdateUser = (userId, username, password) => {
+  const query = "UPDATE users SET user_name=?, user_password=? WHERE user_id=?";
+  return new Promise((resolve, reject) => {
+    database.query(query, [username, password, userId], (error, result) => {
+      if (error) reject(error);
+      resolve(result);
+    });
+  });
+};
+
+module.exports = {
+  getUsers,
+  getUserByName,
+  createUser,
+  deleteUser,
+  UpdateUser,
+};
