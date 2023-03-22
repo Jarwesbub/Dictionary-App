ec8b812cb3139fd1263e22a08753a4e4dc4ebcc8
Ilari Tuovinen <ilari.tuovinen@hotmail.com>
2023-03-22 20:59:09 +0200

    added button to navigate to login activity

--- mobile/app/src/main/java/com/example/dictionary_app/MainActivity.kt
+++ mobile/app/src/main/java/com/example/dictionary_app/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.dictionary_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dictionary_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnGoToLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
