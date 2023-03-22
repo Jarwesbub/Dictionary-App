b551be401cd50a4a0bf76eccb69634b68fcf69f5
Ilari Tuovinen <ilari.tuovinen@hotmail.com>
2023-03-22 19:48:00 +0200

    added loginRoute

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const loginRoute = require("./route/login_route")
 
 const port = 3000;
 
@@ -8,6 +9,8 @@ app.get("/", (req, res) => {
   res.status(200).send({ message: "Kukkuluuruu" });
   res.end;
 });
+
+app.use("/login",loginRoute)
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}...`);
