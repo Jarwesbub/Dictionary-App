eab00d2ce5d31a7e52ec3acbea46ab7da14a3884
henjus <80738089+henjus@users.noreply.github.com>
2023-03-23 02:26:38 +0200

    main view, buttons for it, settings button & icon for toolbar

--- mobile/app/src/main/java/com/example/dictionary_app/MainActivity.kt
+++ mobile/app/src/main/java/com/example/dictionary_app/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.dictionary_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.View
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        //add a settings button on the tools bar
+        menuInflater.inflate(R.menu.menubuttons, menu)
+        return true
+    }
+
+    fun openWordSearch(view: View?) {
+    }
+    fun openFavouriteWords(view: View?) {
+    }
+    fun openFlashCards(view: View?) {
+    }
+    fun openSettings(view: View?) {
     }
 }
