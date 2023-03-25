d1e2c47049641634b442e715c3f3e79b84bdb3f2
Ilari Tuovinen <ilari.tuovinen@hotmail.com>
2023-03-25 20:20:56 +0200

    added to login create and save refresh token

--- server/route/login_route.js
+++ server/route/login_route.js
@@ -10,13 +10,12 @@ router.post("/", async (req, res) => {
 
   try {
     const response = await login.getUserInfo(username);
-    console.log(response);
     const hashedPassword = response[0].user_password;
     if (await bcrypt.compare(password, hashedPassword)) {
       const userId = response[0].user_id;
       const username = response[0].user_name;
       const accessToken = auth.generateAccessToken(userId, username);
-      const refreshToken = auth.generaRefreshToken(userId, username);
+      const refreshToken = await auth.generaRefreshToken(userId, username);
       res
         .status(200)
         .json({ accessToken: accessToken, refreshToken: refreshToken });
