31fabba78a99da132f8f89ca29bffe42b0c71b55
Ilari Tuovinen <ilari.tuovinen@hotmail.com>
2023-03-25 17:01:00 +0200

    updated with unique username check to prevent issues with multiple users with same name.

--- server/route/signup_route.js
+++ server/route/signup_route.js
@@ -1,22 +1,36 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcrypt")
-const user = require("../model/user_model");
+const bcrypt = require("bcrypt");
+const signup = require("../model/signup_model");
 
-router.post("/", async (req,res)=>{
-    try {
-        const salt = await bcrypt.genSalt()
-        const hashedPassword = await bcrypt.hash(req.body.password, salt)
+// This is a route that handles POST requests to create a new user account
+router.post("/", async (req, res) => {
+  try {
+    const username = req.body.username;
 
-        const username = req.body.username;
+    // Get list of all users in database
+    const userList = await signup.getAllUsers();
 
-        const response = await user.createUser(username,hashedPassword)
-        res.status(200).json(response);
+    // Check if given username exists in database
+    const isUsernameTaken = userList.some(
+      (user) => user.user_name === username
+    );
+    // If the username is taken, it sends a 409 (Conflict) response with the message "name taken."
+    if (isUsernameTaken) {
+      res.status(409).send("name taken.");
+    } 
+    // If the username is not taken, proceed with signup by hashing provided password and updating database with user records
+    else {
+      const salt = await bcrypt.genSalt();
+      const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+      const response = await signup.createUser(username, hashedPassword);
+      res.status(200).json(response);
     }
-    catch (error) {
-        console.error(error);
-        res.sendStatus(500);
-      }
-})
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
 
 module.exports = router;
