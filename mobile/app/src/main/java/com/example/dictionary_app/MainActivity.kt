879e6b91830077983ae2d065d9a1e782844a9540
henjus <80738089+henjus@users.noreply.github.com>
2023-03-28 02:55:38 +0300

    Fixed Main's buttons. Made Licences & About Us pages, created menu button functions for them. Placeholder function for Settings menu button.

--- mobile/app/src/main/java/com/example/dictionary_app/MainActivity.kt
+++ mobile/app/src/main/java/com/example/dictionary_app/MainActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 import com.example.dictionary_app.databinding.ActivityMainBinding
 
 
