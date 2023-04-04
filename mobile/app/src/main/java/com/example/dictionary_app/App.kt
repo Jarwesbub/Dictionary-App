88f3ca0bb7cf5d3d12486dcf38d30d4a7072606d
Ilari Tuovinen <ilari.tuovinen@hotmail.com>
2023-04-04 19:36:07 +0300

    created App.kt to enable global usage of preferences

---
+++ mobile/app/src/main/java/com/example/dictionary_app/App.kt
@@ -0,0 +1,22 @@
+package com.example.dictionary_app
+
+import android.app.Application
+
+val prefs: Prefs by lazy {
+    App.prefs!!
+}
+
+class App: Application() {
+
+    companion object{
+        var prefs: Prefs? = null
+        lateinit var instance: App
+        private set
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+        prefs = Prefs(applicationContext)
+    }
+}
