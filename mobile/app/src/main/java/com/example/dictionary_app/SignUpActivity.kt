25aac825f42bf20e568aed9edc6eb3eb5bdfae8b
Ilari Tuovinen <ilari.tuovinen@hotmail.com>
2023-04-08 19:34:11 +0300

    enabled binding

--- mobile/app/src/main/java/com/example/dictionary_app/SignUpActivity.kt
+++ mobile/app/src/main/java/com/example/dictionary_app/SignUpActivity.kt
@@ -2,10 +2,18 @@ package com.example.dictionary_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dictionary_app.databinding.ActivityLoginBinding
+
+
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityLoginBinding;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
