dac6bd76256b04c32b937800424b23d3fcf2d3cf
Ilari Tuovinen <ilari.tuovinen@hotmail.com>
2023-04-08 21:01:42 +0300

    simple signup post function

--- mobile/app/src/main/java/com/example/dictionary_app/SignUpActivity.kt
+++ mobile/app/src/main/java/com/example/dictionary_app/SignUpActivity.kt
@@ -3,6 +3,13 @@ package com.example.dictionary_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.dictionary_app.databinding.ActivitySignUpBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.OutputStreamWriter
+import java.net.HttpURLConnection
+import java.net.URL
 
 
 class SignUpActivity : AppCompatActivity() {
@@ -14,5 +21,59 @@ class SignUpActivity : AppCompatActivity() {
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        binding.btnSignUp.setOnClickListener { onSignUpClick() }
+    }
+
+    private fun onSignUpClick() {
+        val username = binding.etSignUpUsername.text.toString()
+        val password = binding.etSignUpPassword.text.toString()
+
+        CoroutineScope(Dispatchers.Main).launch {
+            try {
+                val result = signUpRequest(username,password)
+                println(result)
+            }
+            catch (e: Exception){
+            println(e.message)
+            }
+        }
+    }
+    private suspend fun signUpRequest(username: String, password: String) = withContext(Dispatchers.IO){
+        // Create a URL object with the URL we want to connect to
+        val url = URL("http://{server ip here}:3000/signup")
+
+        // Open an HTTP connection to the URL
+        val conn = url.openConnection() as HttpURLConnection
+
+        // Set the request method to POST, and enable output and input for the connection
+        conn.requestMethod = "POST"
+        conn.doOutput = true
+        conn.doInput = true
+
+        // Set the Content-Type header to indicate that we're sending a JSON payload
+        conn.setRequestProperty("Content-Type", "application/json")
+
+        // Create a JSON payload with the username and password
+        val body = "{ \"username\": \"$username\", \"password\": \"$password\" }"
+
+        // Write the payload to the output stream of the connection
+        val output = OutputStreamWriter(conn.outputStream)
+        output.write(body)
+        output.flush()
+
+        // Check the response code to see if the request was successful
+        val responseCode = conn.responseCode
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            // Request successful - read the response data into a string and print it to the console
+            val response = conn.inputStream.bufferedReader().use { it.readText() }
+            println(response)
+        } else {
+            // Request unsuccessful - print an error message with the response code
+            println("Error: $responseCode")
+        }
+
+        // Disconnect the connection to free up system resources
+        conn.disconnect()
     }
 }
