17776ebd8913fff640a5bee1b2d654a0b079b7ae
Ilari Tuovinen <ilari.tuovinen@hotmail.com>
2023-04-20 00:57:19 +0300

    made function async and added db check

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -53,10 +53,17 @@ verifyAccessToken = (req, res, next) => {
     next();
   });
 };
-verifyRefreshToken = (token) => {
+verifyRefreshToken = async (token) => {
   try {
     const decoded = jwt.verify(token, process.env.REFRESH_TOKEN_SECRET);
-    return decoded;
+    const userId = decoded.userId;
+
+    const dbToken = await authModel.getRefreshToken(userId)
+
+    console.log(dbToken)
+    console.log(typeof(token))
+    if(token===dbToken[0].refresh_token) return decoded;
+    else throw new Error("Invalid refresh token");
   } catch (error) {
     console.error(error);
     throw new Error("Invalid refresh token");
