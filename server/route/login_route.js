cf1a1ff68d08521e8acc746bfe1293634d58804c
Ilari Tuovinen <ilari.tuovinen@hotmail.com>
2023-04-20 00:16:24 +0300

    added route to login with refresh token

--- server/route/login_route.js
+++ server/route/login_route.js
@@ -16,20 +16,42 @@ router.post("/", async (req, res) => {
       const username = response[0].user_name;
       const accessToken = auth.generateAccessToken(userId, username);
       const refreshToken = await auth.generaRefreshToken(userId, username);
-      res
-        .status(200)
-        .json({
-          user_id: userId,
-          user_name: username,
-          accessToken: accessToken,
-          refreshToken: refreshToken,
-        });
+      res.status(200).json({
+        user_id: userId,
+        user_name: username,
+        accessToken: accessToken,
+        refreshToken: refreshToken,
+      });
     } else {
       res.status(401).send("Not allowed");
     }
   } catch (error) {
     console.error(error);
     res.sendStatus(500);
+  }
+});
+
+router.post("/token", async (req, res) => {
+  const refreshToken = req.body.refreshToken;
+  try {
+    const response = await auth.verifyRefreshToken(refreshToken);
+    const userId = response.userId;
+    const username = response.username;
+    const accessToken = await auth.generateAccessToken(userId, username);
+    const newRefreshToken = await auth.generaRefreshToken(userId, username);
+    res.status(200).json({
+      user_id: userId,
+      user_name: username,
+      accessToken: accessToken,
+      refreshToken: newRefreshToken,
+    });
+  } catch (error) {
+    if (error.message === "Invalid refresh token") {
+      res.status(401).send("Invalid refresh token");
+    } else {
+      console.error(error);
+      res.status(500).send("Internal server error");
+    }
   }
 });
 
