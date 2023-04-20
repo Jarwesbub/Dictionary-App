b5cd51c3c60e8483e4ab766cd820876a85d5e541
henjus <80738089+henjus@users.noreply.github.com>
2023-04-20 19:17:58 +0300

    custom theme for alert dialog

--- mobile/app/src/main/java/com/example/dictionary_app/SettingsActivity.kt
+++ mobile/app/src/main/java/com/example/dictionary_app/SettingsActivity.kt
@@ -2,6 +2,7 @@ package com.example.dictionary_app
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.ContextThemeWrapper
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
@@ -193,7 +194,7 @@ class SettingsActivity : AppCompatActivity() {
         Toast.makeText(this@SettingsActivity, message, Toast.LENGTH_SHORT).show()
     }
     private fun deleteUserAlert(){
-        val builder = AlertDialog.Builder(this)
+        val builder = AlertDialog.Builder(ContextThemeWrapper(this, R.style.AlertDialogCustom))
         with(builder) {
             setTitle("Do you want to delete your account?")
             setMessage("If you confirm you will be automatically logged out and won't be able to access your account and associated data anymore.")
