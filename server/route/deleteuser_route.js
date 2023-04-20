89d19644e28af5ccb232fc7678fe8efac9d3cdf6
henjus <80738089+henjus@users.noreply.github.com>
2023-04-20 16:28:30 +0300

    reworked to use access token to check auth for deleting user

--- server/route/deleteuser_route.js
+++ server/route/deleteuser_route.js
@@ -1,33 +1,28 @@
 const express = require("express");
 const router = express.Router();
 const userModel = require("../model/user_model");
+const auth = require("../utils/auth");
 const login = require("../model/login_model");
 
 router.delete("/", async (req, res) => {
-    const username = req.body.username;
-    const refreshToken = req.body.refresh_token;
-    console.log(refreshToken);
-    
+    await auth.verifyAccessToken(req, res);
+    const username = req.user.username;
+    const userId = req.user.userId;
+    if (req.user != null && req.body.username == req.user.username) {
     try {
-      const response = await login.getUserInfo(username);
-      if (response != null) {
-        const userId = response[0].user_id;
-        if (userId != null) {
-            userModel.deleteUser(userId);
-            res
-            .status(200)
-            .send("User deleted");
-        } else {        
-        res
-        .status(500)
-        .send("Failed to delete tokens");
+          if (userId != null) {
+              userModel.deleteUser(userId);
+              res.status(200).send("User deleted");
+              console.log("user account "+username+" deleted");
+          } else {        
+          res.status(500).send("Failed to delete user");
+          }
+        } catch (error) {
+        console.error(error);
+        res.sendStatus(500);
         }
       } else {
-        res.status(401).send("Not allowed");
-      }
-    } catch (error) {
-      console.error(error);
-      res.sendStatus(500);
+      res.status(401).send("Not allowed");
     }
   });
   
